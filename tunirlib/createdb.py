2fe2a186c04a22e5b7b9181ef14041015cb07de5
Kushal Das <kushaldas@gmail.com>
2015-01-17 01:16:58 +0530

    Fixes for the module move.

--- tunirlib/createdb.py
+++ tunirlib/createdb.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
 
 # These two lines are needed to run on EL6
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
-from tunirlib import default_config
-from tunirlib import model
+import default_config
+import model
 
 model.create_tables(
     default_config.DB_URL,

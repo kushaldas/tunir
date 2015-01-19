b9ae91fc86bb39c114ad10bc3fcaf4309e6bf304
Kushal Das <kushaldas@gmail.com>
2015-01-19 20:39:52 +0530

    Example script to add many local ports to the queue.

---
+++ createports.py
@@ -0,0 +1,8 @@
+import redis
+
+ports = [2222, 2223, 2224,2225,2226, 2227, 2228, 2229, 2230]
+
+if __name__ == '__main__':
+    r = redis.Redis()
+    for p in ports:
+        r.lpush('tunirports', p)

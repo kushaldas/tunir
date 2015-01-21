51c19777188eaf4f40e6cb2d22f6e9f6cb8003c2
Kushal Das <kushaldas@gmail.com>
2015-01-21 21:41:26 +0530

    moving to 0.2

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import find_packages, setup
 
 setup(name = 'tunir',
-    version = '0.1',
+    version = '0.2',
     description = "Simple CI system.",
     long_description = "A simple CI system which can be maintained.",
     platforms = ["Linux"],
     author="Kushal Das",
     author_email="kushaldas@gmail.com",
     url="http://tunir.rtfd.org",
-    license = "GPL",
+    license = "GPLv2+",
     packages=find_packages(),
     data_files=[('share/tunir',
                   ['createports.py', 'default.json',

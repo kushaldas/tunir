d45a2147fe5ce13cf12c631c9da9210ff03f93bd
Kushal Das <kushaldas@gmail.com>
2015-10-08 14:18:47 +0530

    Release 0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name = 'tunir',
-    version = '0.8',
+    version = '0.9',
     description = "Simple CI system.",
     long_description = "A simple CI system which can be maintained.",
     platforms = ["Linux"],

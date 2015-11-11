10120a00dde7e3df3283a9e479416cbd2138226d
Kushal Das <kushaldas@gmail.com>
2015-11-11 12:03:21 +0530

    Now we test execute function.

--- tests.py
+++ tests.py
@@ -1,6 +1,20 @@
 import unittest
-import mock
+from mock import patch
+import sys
 import tunirlib
+from contextlib import contextmanager
+from StringIO import StringIO
+from tunirlib.tunirdocker import Result
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
 
 
 class TunirTests(unittest.TestCase):
@@ -13,6 +27,42 @@ class TunirTests(unittest.TestCase):
         self.assertEqual(2048, data["ram"], "Missing ram information")
 
 
+class ExecuteTests(unittest.TestCase):
+    """
+    Tests the execute function.
+    """
+    @patch('tunirlib.run')
+    def test_execute(self, t_run):
+        config = {"host_string": "127.0.0.1",
+                  "user": "fedora"}
+        r1 = Result("result1")
+        r1.return_code = 0
+        t_run.return_value=r1
+        res, negative = tunirlib.execute(config, 'ls')
+        self.assertEqual(res, "result1")
+        self.assertEqual(negative, "no")
+
+    @patch('tunirlib.run')
+    def test_execute_nagative(self, t_run):
+        config = {"host_string": "127.0.0.1",
+                  "user": "fedora"}
+        r1 = Result("result1")
+        r1.return_code = 127
+        t_run.return_value=r1
+        res, negative = tunirlib.execute(config, '@@ ls')
+        self.assertEqual(res, "result1")
+        self.assertEqual(negative, "yes")
+
+    @patch('tunirlib.run')
+    def test_execute_nongating(self, t_run):
+        config = {"host_string": "127.0.0.1",
+                  "user": "fedora"}
+        r1 = Result("result1")
+        r1.return_code = 127
+        t_run.return_value=r1
+        res, negative = tunirlib.execute(config, '## ls')
+        self.assertEqual(res, "result1")
+        self.assertEqual(negative, "dontcare")
 
 if __name__ == '__main__':
     unittest.main()

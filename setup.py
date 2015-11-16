77ec582520303799f0e211ab3125076410300bd2
Kushal Das <kushaldas@gmail.com>
2015-11-16 09:54:01 +0530

    Getting ready for 0.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name = 'tunir',
-    version = '0.11',
+    version = '0.12',
     description = "Simple CI system.",
     long_description = "A simple CI system which can be maintained.",
     platforms = ["Linux"],

b1e939513b5c98bb0685451b345ec985d2c3d2d3
Kushal Das <mail@kushaldas.in>
2016-03-30 21:18:01 +0530

    Updates to 0.15.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name = 'tunir',
-    version = '0.15',
+    version = '0.15.1',
     description = "Simple testing system.",
     long_description = "A simple testing system which can be maintained.",
     platforms = ["Linux"],

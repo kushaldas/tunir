0611aa81603b968c759c893606162ba6352901f5
Kushal Das <mail@kushaldas.in>
2016-03-30 17:50:36 +0530

    Updates to 0.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name = 'tunir',
-    version = '0.13',
+    version = '0.14',
     description = "Simple CI system.",
     long_description = "A simple CI system which can be maintained.",
     platforms = ["Linux"],
@@ -13,7 +13,7 @@ setup(name = 'tunir',
     license = "GPLv2+",
     packages=find_packages(),
     data_files=[('share/tunir',
-                  ['createports.py', 'default.json',
+                  ['default.json', 'multihost.txt','multihost.cfg'
                     'default.txt', 'vgt.json', 'vgt.txt']),
         ('share/man/man8/', ['tunir.8'])],
     entry_points = {

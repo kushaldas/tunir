20bb5b2f5296280bd7cff82c4d721e4c86c65e4b
Kushal Das <mail@kushaldas.in>
2016-05-10 13:05:10 +0530

    Closes #44 non-gating count is back to normal

--- tunirlib/tunirutils.py
+++ tunirlib/tunirutils.py
@@ -310,7 +310,7 @@ def run_job(jobpath, job_name='', extra_config={}, container=None,
             for key, value in STR.iteritems():
                 fobj.write("command: %s\n" % value['command'])
                 print "command: %s" % value['command']
-                if value['command'].find(' ## ') != -1:
+                if value['command'].find(' ## ') != -1 or value['command'].startswith('## '):
                     nongating['number'] += 1
                     if value['status'] == False:
                         nongating['fail'] += 1

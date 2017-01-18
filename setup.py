c9dc5f1cddf33e40c38f1694b03f81f8ec2099ed
Kushal Das <mail@kushaldas.in>
2017-01-18 13:39:19 +0530

    Updates to 0.16.1 bugfix release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name = 'tunir',
-    version = '0.16',
+    version = '0.16.1',
     description = "Simple testing system.",
     long_description = "A simple testing system which can be maintained.",
     platforms = ["Linux"],
